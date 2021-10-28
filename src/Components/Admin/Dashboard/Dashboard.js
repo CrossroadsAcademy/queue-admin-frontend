7c114ac7be42e94fda6379dc44888b65e1d22490
illias <illiasibrahim6@gmail.com>
2021-10-29 00:26:37 +0530

    folder structure completed, ready to divide

---
+++ src/Components/Admin/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import classes from './Dashboard.module.css';
+
+const Dashboard = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ src/Components/Admin/Profile/Profile.js
@@ -0,0 +1,9 @@
+import classes from "./Profile.module.css";
+
+const Profile = () => {
+  return <div>
+      
+  </div>;
+};
+
+export default Profile;
