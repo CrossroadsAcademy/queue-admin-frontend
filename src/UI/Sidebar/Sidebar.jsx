551c6f5a0eb98e6439fe216c3fbc778a5faa0e4b
Nihal-a <nihalavulan1@gamil.com>
2022-10-15 23:35:50 +0530

    list added and styled

--- src/UI/Sidebar/Sidebar.jsx
+++ src/UI/Sidebar/Sidebar.jsx
@@ -1,38 +1,49 @@
-import React from 'react'
-import classes from './sidebar.module.css'
-import { Icon } from '@iconify/react';
+import React from "react";
+import classes from "./sidebar.module.css";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
 
 const Sidebar = () => {
 
+    const [active, setActive] = useState(1)
 
-
-    const sideBarItems = [
-        {
-            name : 'Dashboard',
-            icon : 'ant-design:dashboard-outlined'
-        },
-        {
-            name : 'Set Up Queue',
-            icon : 'clarity:users-line'
-        },
-        {
-            name : 'Staff',
-            icon : 'clarity:employee-line'
-        },
-        {
-            name : 'Profile',
-            icon : 'iconoir:profile-circled'
-        }
-    ]   
+  const sideBarItems = [
+    {
+        id : 1,
+        name: "Dashboard",
+      icon: "ant-design:dashboard-outlined",
+      link : '/'
+    },
+    {
+        id : 2,
+        name: "Set Up Queue",
+      icon: "clarity:users-line",
+      link : '/queue'
+    },
+    {
+        id : 3,
+        name: "Staff",
+      icon: "clarity:employee-line",
+      link : '/staff'
+    },
+    {
+        id : 4,
+        name: "Profile",
+      icon: "iconoir:profile-circled",
+      link : '/profile'
+    },
+  ];
 
   return (
     <div className={classes.wrapper}>
-                <a href="" className={classes.listItem}>
-                    <Icon icon='ant-design:dashboard-outlined' />
-                    <span>Dashboard</span>
-                </a>
+      {sideBarItems.map((item) => (
+        <a href={item.link} onClick={() => setActive(item.id)} className={classes.listItem}>
+          <Icon icon={item.icon} width={26} />
+          <span>{item.name}</span>
+        </a>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
