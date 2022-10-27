4a3d7f1edfd42361033e87f461a671087add8de0
nihalavulan <nihalavulan1@gmail.com>
2022-10-27 12:34:35 +0530

    Added laoding state on routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Test from './Test/Test';
+import { Suspense } from 'react';
+import CssSpinner from './Test/CssSpinner/CssSpinner';
+
 function App() {
   return (
-    <Test />
+     <Suspense fallback={<CssSpinner />}>
+       <Test />
+     </Suspense>
   );
 }
+
+
 
 export default App;
---
+++ src/Test/CssSpinner/CssSpinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './CssSpinner.module.css'
+
+const CssSpinner = () => {
+  return (
+    <div id={styles.loading}></div>
+  )
+}
+
+export default CssSpinner
