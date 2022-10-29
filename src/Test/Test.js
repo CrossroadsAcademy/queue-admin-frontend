faff89b7ace0ce2e724ecf3681f66d5ff917d121
amsadams <anandu.email@gmail.com>
2022-10-29 20:55:19 +0530

    code pulled

--- src/Test/Test.js
+++ src/Test/Test.js
@@ -1,10 +1,11 @@
 import classes from "./Test.module.css";
-
+import CssSpinner from './CssSpinner/CssSpinner'
 const Test = () => {
   return (
     <div className={classes.container}>
       <div className={classes["test-space"]}>
         <h1>Test space</h1>
+        <CssSpinner/>
       </div>
     </div>
   );
